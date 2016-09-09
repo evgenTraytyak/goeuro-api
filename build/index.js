18f217581e7b0fa3672aea4d0dd531ad8c9c5c83
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-09-09 10:24:17 +0300

    Add lib and build folder. Create GoEuroAPI class

---
+++ build/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _unirest = require('unirest');
+
+var _unirest2 = _interopRequireDefault(_unirest);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var GoEuroAPI = function GoEuroAPI() {
+  _classCallCheck(this, GoEuroAPI);
+};
+
+exports.default = GoEuroAPI;
